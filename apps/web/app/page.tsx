9b6a4195322224c0b672519feb59ce66d56cdab2
Jiashu Zou <zjsdut@163.com>
2024-02-13 11:49:38 +0800

    Make MarkSlides editor a component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,77 +1,21 @@
 'use client';
 
-import { useState } from 'react';
-import MarkSlidesEditor from '@markslides/editor';
-import {
-    undo,
-    redo,
-    heading,
-    bold,
-    italic,
-    strike,
-    underline,
-    blockQuotes,
-    orderedList,
-    unorderedList,
-    todoList,
-    link,
-    image,
-    code,
-    codeBlock,
-    mermaid,
-} from '@markslides/editor/toolbar';
-import type { SlideInfo } from '@markslides/editor';
-// import type { ToolbarCommand } from '@markslides/editor/toolbar';
+import MarkSlides from '../components/MarkSlides'
 
 function Page(): JSX.Element {
-    const [value, setValue] = useState('');
+    const md =
+        `# Slide 1
 
-    const [slideInfo, setSlideInfo] = useState<SlideInfo>({
-        title: '',
-        currentSlideTitle: '',
-        currentSlideNumber: 0,
-        totalSlideCount: 0,
-    });
+---
 
-    return (
-        <MarkSlidesEditor
-            toolbarCommands={[
-                undo,
-                redo,
-                heading,
-                bold,
-                italic,
-                strike,
-                underline,
-                blockQuotes,
-                orderedList,
-                unorderedList,
-                todoList,
-                link,
-                image,
-                code,
-                codeBlock,
-                mermaid,
-            ]}
-            config={{
-                header: 'MarkSlides',
-                footer: 'Soaple',
-                paginate: true,
-                theme: 'default',
-                class: 'invert',
-                size: '16:9',
-            }}
-            isFixScrollToBottom={false}
-            slideInfo={slideInfo}
-            onChangeSlideInfo={(newSlideInfo) => {
-                setSlideInfo(newSlideInfo);
-            }}
-            value={value}
-            onChange={(newValue) => {
-                setValue(newValue);
-            }}
-        />
-    );
+# Slide 2
+
+---
+
+# Slide 3
+
+`
+    return <MarkSlides md={md} />;
 }
 
 export default Page;
---
+++ apps/web/components/MarkSlides.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import { useState } from 'react';
+import MarkSlidesEditor from '@markslides/editor';
+import {
+    undo,
+    redo,
+    heading,
+    bold,
+    italic,
+    strike,
+    underline,
+    blockQuotes,
+    orderedList,
+    unorderedList,
+    todoList,
+    link,
+    image,
+    code,
+    codeBlock,
+    mermaid,
+} from '@markslides/editor/toolbar';
+import type { SlideInfo } from '@markslides/editor';
+// import type { ToolbarCommand } from '@markslides/editor/toolbar';
+
+
+function MarkSlides({ md = '' }): JSX.Element {
+    const [value, setValue] = useState(md);
+
+    const [slideInfo, setSlideInfo] = useState<SlideInfo>({
+        title: '',
+        currentSlideTitle: '',
+        currentSlideNumber: 0,
+        totalSlideCount: 0,
+    });
+
+    return (
+        <MarkSlidesEditor
+            toolbarCommands={[
+                undo,
+                redo,
+                heading,
+                bold,
+                italic,
+                strike,
+                underline,
+                blockQuotes,
+                orderedList,
+                unorderedList,
+                todoList,
+                link,
+                image,
+                code,
+                codeBlock,
+                mermaid,
+            ]}
+            config={{
+                header: 'MarkSlides',
+                footer: 'Soaple',
+                paginate: true,
+                theme: 'default',
+                class: 'invert',
+                size: '16:9',
+            }}
+            isFixScrollToBottom={false}
+            slideInfo={slideInfo}
+            onChangeSlideInfo={(newSlideInfo) => {
+                setSlideInfo(newSlideInfo);
+            }}
+            value={value}
+            onChange={(newValue) => {
+                setValue(newValue);
+            }}
+        />
+    );
+}
+
+export default MarkSlides;
